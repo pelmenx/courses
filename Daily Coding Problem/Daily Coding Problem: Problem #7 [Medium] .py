d156a61b93b1daf6d42d8b6853e2605df0cf119c
pelmenx <vtalantsev@gmail.om>
2020-12-07 23:10:06 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #7 [Medium] .py
+++ Daily Coding Problem/Daily Coding Problem: Problem #7 [Medium] .py
@@ -15,25 +15,60 @@
 # --------------------------------------------------------------------------------
 #
 #
-def encoder(encode_text):
+def encoder(encode_text, text=[], tmp_text1=[]):
     map = {"1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f", "7": "g", "8": "h", "9": "i",
            "10": "j", "11": "k", "12": "l", "13": "m", "14": "n", "15": "o", "16": "p", "17": "q",
            "18": "r", "19": "s", "20": "t", "21": "u", "22": "v", "23": "w", "24": "x", "25": "y", "26": "z"}
     if len(encode_text) == 1:
         count = 1
-        print(encode_text)
+
+        text.append(encode_text)
+        tmp_text1.extend(text)
+        text = tmp_text1
+        print("!", text)
+        for i in text:
+            print(map[i], end='')
+        print()
+        text = []
     elif len(encode_text) == 2:
         if int(encode_text) >= 1 and int(encode_text) <= 26:
             count = 2
-            print(encode_text[0], encode_text[1])
-            print(encode_text)
+
+            tmp_text = text.copy()
+            text.append(encode_text[0])
+            text.append(encode_text[1])
+            print("@", text)
+            for i in text:
+                print(map[i], end='')
+            print()
+            text = []
+            text.extend(tmp_text)
+            text.append(encode_text)
+            print("#", text)
+            for i in text:
+                print(map[i], end='')
+            print()
+            text = []
         else:
             count = 1
+
             print(encode_text)
+            text.append(encode_text[0])
+            text.append(encode_text[1])
+            print("%", text)
+            for i in text:
+                print(map[i], end='')
+            print()
+            text = []
     else:
-        count = encoder(encode_text[1:])
+
+        tmp_text1 = text.copy()
+        text.append(encode_text[:1])
+        count = encoder(encode_text[1:], text, tmp_text1)
+        text = []
         if int(encode_text[:2]) >= 1 and int(encode_text[:2]) <= 26:
-            count += encoder(encode_text[2:])
+            text.append(encode_text[:2])
+            count += encoder(encode_text[2:], text, tmp_text1)
 
     return count
 

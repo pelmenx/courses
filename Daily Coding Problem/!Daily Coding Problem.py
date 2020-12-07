f97212e35c7055355e3af8c0a6c6dc1e9374d4a0
pelmenx <vtalantsev@gmail.om>
2020-12-07 16:51:20 +0300

    update

--- Daily Coding Problem/!Daily Coding Problem.py
+++ Daily Coding Problem/!Daily Coding Problem.py
@@ -33,7 +33,7 @@ for id in id_list:
         # print(body)
     body = body.split("\n")
     try:
-        with open("./gmail/" + file_name + '.py', 'x') as file:
+        with open("./Daily Coding Problem/" + file_name + '.py', 'x') as file:
             for line in body:
                 if len(line) > 1:
                     file.write("# " + line + '\n')

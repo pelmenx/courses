861b3036ff7ffc67c7c3aaaa45f3424bfea7426e
pelmenx <vtalantsev@gmail.om>
2020-12-16 23:39:20 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #19 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #19 [Medium].py
@@ -14,3 +14,47 @@
 # --------------------------------------------------------------------------------
 #
 #
+import sys
+
+
+def find_all_pairs(matrix, counter):
+    global list, tmp_list
+    if counter > 0:
+        for i in range(0, len(matrix[counter])):
+            tmp_list.append([i, matrix[counter][i]])
+            find_all_pairs(matrix, counter - 1)
+            tmp_list.pop()
+
+    else:
+        for j in range(0, len(matrix[counter])):
+            tmp_list.append([j, matrix[counter][j]])
+            # print(tmp_list)
+            list.append(tmp_list.copy())
+            tmp_list.pop()
+    return list
+
+
+def find_min_cost_for_houses(pairs):
+    cost = sys.maxsize
+    for pair in pairs:
+        tmp_cost = 0
+        for i in range(0, len(pair)):
+            tmp_cost += pair[i][1]
+            if i < len(pair) - 1:
+                if pair[i][0] == pair[i + 1][0]:
+                    break
+            if i == len(pair) - 1:
+                if tmp_cost < cost:
+                    cost = tmp_cost
+    return cost
+
+
+N_by_K_matrix = [[7, 3, 8, 6, 1, 2],
+                 [5, 6, 7, 2, 4, 3],
+                 [10, 1, 4, 9, 7, 6],
+                 [10, 1, 4, 9, 7, 6]]
+
+list = []
+tmp_list = []
+
+print(find_min_cost_for_houses(find_all_pairs(N_by_K_matrix, len(N_by_K_matrix) - 1)))

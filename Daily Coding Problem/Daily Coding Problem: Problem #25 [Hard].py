519988629a88bf3a6a66dc04aadede2f3dd81050
pelmenx <vtalantsev@gmail.om>
2020-12-22 00:48:16 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #25 [Hard].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #25 [Hard].py
@@ -22,21 +22,65 @@
 # --------------------------------------------------------------------------------
 #
 #
-def regexp_validator(regexp, string):
-    #print(regexp, string)
-    if regexp[0] != "*":
-        if len(regexp) == 1:
-            if len(string) == 1:
-                return True
-            else:
-                return False
-        if regexp[0] == string[0] or regexp[0] == ".":
-            return regexp_validator(regexp[1:], string[1:])
+import sys
+
+
+def check_list():
+    global list
+    if True in list:
+        list = []
+        return True
     else:
+        list = []
+        return False
+
+
+def regexp_validator(regexp, string):
+    global list
+    try:
+        if regexp[0] != "*":
+            if len(regexp) == 1:
+                if len(string) == 1:
+                    list.append(True)
+            else:
+                if regexp[0] == string[0] or regexp[0] == ".":
+                    return regexp_validator(regexp[1:], string[1:])
+
+        else:
+            counter = 0
+            count = 0
+            for symbols in regexp:
+                if symbols == "." or symbols == "*":
+                    counter += 1
+                    if symbols == ".":
+                        count += 1
+            if counter == len(regexp) and count <= len(string):
+                list.append(True)
+            else:
+                min_i = sys.maxsize
+                for i in range(0, len(regexp)):
+                    if regexp[i] != "." and regexp[i] != "*" and i < min_i:
+                        min_i = i
+
+                for j in range(0, len(string)):
+                    if regexp[min_i] == string[j]:
+                        regexp_validator(regexp[min_i:], string[j:])
+    except IndexError:
         pass
 
 
-print(regexp_validator("ra.", "ray"))
-print(regexp_validator("ra.", "raymond"))
-print(regexp_validator(".*at", "chat"))
-print(regexp_validator(".*at", "chats"))
+list = []
+regexp_validator("ra.", "ray")
+print(check_list())
+regexp_validator("ra.", "raymond")
+print(check_list())
+regexp_validator(".*at", "chatat")
+print(check_list())
+regexp_validator(".*at", "chats")
+print(check_list())
+regexp_validator(".*", "chhat")
+print(check_list())
+regexp_validator("*******t", "chhat")
+print(check_list())
+regexp_validator(".*..a**.", "chhat")
+print(check_list())

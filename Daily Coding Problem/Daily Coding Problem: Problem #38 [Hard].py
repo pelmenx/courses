907f06ae7a28335306e106838cd420b7d0d9730c
pelmenx <vtalantsev@gmail.om>
2021-01-02 00:47:50 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #38 [Hard].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #38 [Hard].py
@@ -11,3 +11,30 @@
 # --------------------------------------------------------------------------------
 #
 #
+import copy
+
+
+def queens_position(n, matrix=[], counter=None):
+    if not matrix:
+        matrix = []
+        for i in range(n):
+            matrix.append([])
+            for j in range(n):
+                matrix[i].append(None)
+        tmp_matrix = copy.deepcopy(matrix)
+        counter = 0
+        return queens_position(n, tmp_matrix, counter)
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] is None:
+                    matrix[i][j] = 1
+                    counter += 1
+                    for k in range(len(matrix[i])):
+                        if k != j:
+                            matrix[i][k] = 0
+                            matrix[k][j] = 0
+        print(matrix)
+        print
+
+queens_position(8)

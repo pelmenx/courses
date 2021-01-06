05aca96eac624f91a2555d5a442c0642113a4a56
pelmenx <vtalantsev@gmail.om>
2021-01-07 00:28:35 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #41 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #41 [Medium].py
@@ -24,3 +24,21 @@
 # --------------------------------------------------------------------------------
 #
 #
+def find_itinerary(list_of_flights, starting_airport, path=[]):
+    if not list_of_flights:
+        return path + [starting_airport]
+    itinerary = None
+    for i in range(len(list_of_flights)):
+        if list_of_flights[i][0] == starting_airport:
+            current_itinerary = find_itinerary(list_of_flights[:i] + list_of_flights[i + 1:], list_of_flights[i][1], path + [list_of_flights[i][0]])
+            if itinerary is None:
+                itinerary = current_itinerary
+            else:
+                if current_itinerary < itinerary:
+                    itinerary = current_itinerary
+    return itinerary
+
+
+print(find_itinerary([('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')], 'YUL'))
+print(find_itinerary([('SFO', 'COM'), ('COM', 'YYZ')], 'COM'))
+print(find_itinerary([('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')], 'A'))

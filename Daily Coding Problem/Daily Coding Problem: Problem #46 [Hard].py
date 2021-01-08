0e975fa6f9e9b5f98800f972e4ff6b81b3858a0f
pelmenx <vtalantsev@gmail.om>
2021-01-08 22:25:09 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #46 [Hard].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #46 [Hard].py
@@ -15,11 +15,11 @@
 def find_the_longest_palindrom(string):
     if not string:
         return
+    lengest_polindrom = ""
     polindrom = is_palindrom(string)
     if polindrom:
         return string
     polindroms = (find_the_longest_palindrom(string[:-1]), find_the_longest_palindrom(string[1:]))
-    lengest_polindrom = ""
     for item in polindroms:
         if item is not None:
             if len(item) > len(lengest_polindrom):
@@ -31,10 +31,13 @@ def is_palindrom(string):
     half = int(len(string) / 2)
     if len(string) % 2 == 1:
         if string[:half] == string[:half:-1]:
-            return string, len(string)
+            return string
     else:
         if string[:half] == string[:half - 1:-1]:
-            return string, len(string)
+            return string
 
 
 print(find_the_longest_palindrom("aabcdcb"))
+print(find_the_longest_palindrom("bananas"))
+print(find_the_longest_palindrom("a"))
+print(find_the_longest_palindrom(""))

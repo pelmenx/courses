5e93d5eaef65db069bf07778085857a666cc328f
pelmenx <vtalantsev@gmail.om>
2021-01-09 20:18:05 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #48 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #48 [Medium].py
@@ -26,3 +26,29 @@
 # --------------------------------------------------------------------------------
 #
 #
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def get_tree(preorder, inorder):
+    root = preorder[0]
+    if not preorder or not inorder:
+        return None
+    if len(preorder) == 1:
+        return Node(root)
+    root_node = Node(root)
+    for i, char in enumerate(inorder):
+        if char == root:
+            root_node.left = get_tree(preorder[1:i + 1], inorder[:i])
+            root_node.right = get_tree(preorder[i + 1:], inorder[i + 1:])
+    return root_node
+
+
+tree = get_tree(['a', 'b', 'd', 'e', 'c', 'f', 'g'], ['d', 'b', 'e', 'a', 'f', 'c', 'g'])
+
+print(" " * 5, tree.val)
+print(" " * 1, tree.left.val, " " * 5, tree.right.val)
+print(tree.left.left.val, tree.left.right.val, tree.right.left.val, tree.right.right.val, sep="   ")

0862bc1a327ede206c998d9d1b5fe6532da0be0f
pelmenx <vtalantsev@gmail.om>
2021-01-15 15:57:34 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #58 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #58 [Medium].py
@@ -16,3 +16,27 @@
 # --------------------------------------------------------------------------------
 #
 #
+def find_position_of_element(array, number, start=None, end=None):
+    if end is None:
+        end = len(array) - 1
+        start = 0
+    mid = (end + start) // 2
+
+    if array[mid] == number:
+        return mid
+    elif array[mid] < number:
+        if array[start] >= number:
+            return find_position_of_element(array, number, mid + 1, end)
+        else:
+            return find_position_of_element(array, number, start, mid)
+    else:
+        if array[start] <= number:
+            return find_position_of_element(array, number, start, mid)
+        else:
+            return find_position_of_element(array, number, mid + 1, end)
+
+
+print(find_position_of_element([13, 18, 25, 2, 8, 10], 2))
+print(find_position_of_element([13, 18, 25, 2, 8, 10], 8))
+print(find_position_of_element([25, 2, 8, 10, 13, 18], 8))
+print(find_position_of_element([8, 10, 13, 18, 25, 2], 8))

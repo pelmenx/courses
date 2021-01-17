88deef305fd430f7a30250b22079794886a9180e
pelmenx <vtalantsev@gmail.om>
2021-01-17 21:33:46 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #65 [Easy].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #65 [Easy].py
@@ -40,3 +40,31 @@
 # --------------------------------------------------------------------------------
 #
 #
+def clockwise_spiral(matrix):
+    if not matrix:
+        return
+    while matrix:
+        # right
+        for item in matrix[0]:
+            print(item)
+        matrix = matrix[1:]
+        # down
+        for row in matrix:
+            print(row[-1])
+            row.pop()
+        # left
+        if matrix:
+            for item in matrix[-1][::-1]:
+                print(item)
+            matrix = matrix[:-1]
+        # up
+        for row in matrix[::-1]:
+            print(row[0])
+            row.pop(0)
+
+
+matrix = [[1, 2, 3, 4, 5],
+          [6, 7, 8, 9, 10],
+          [11, 12, 13, 14, 15],
+          [16, 17, 18, 19, 20]]
+clockwise_spiral(matrix)

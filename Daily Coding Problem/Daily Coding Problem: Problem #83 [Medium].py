cf7b278bfb8451c76f9a623888da1c9b098b4da8
pelmenx <vtalantsev@gmail.om>
2021-01-22 13:54:54 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #83 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #83 [Medium].py
@@ -26,3 +26,40 @@
 # --------------------------------------------------------------------------------
 #
 #
+class Node(object):
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.arg = arg
+        self.left = None
+        self.right = None
+
+    def invert(root):
+        if not root.right and not root.left:
+            return
+        root.left, root.right = root.right, root.left
+        if root.left:
+            Node.invert(root.left)
+        if root.right:
+            Node.invert(root.right)
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+f = Node(6)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+
+Node.invert(a)
+
+assert a.left == c
+assert a.right == b
+assert c.right == f
+assert b.left == e
+assert b.right == d

6ec93ee75c910ca5676759f98bf8373c0b7a2ba5
pelmenx <vtalantsev@gmail.om>
2021-01-23 12:52:53 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #88 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #88 [Medium].py
@@ -10,3 +10,18 @@
 # --------------------------------------------------------------------------------
 #
 #
+def division(a, b):
+    if a == b:
+        return 1
+    dev = 0
+    c = b
+    while a >= c:
+        c += b
+        dev += 1
+    return dev
+
+
+assert division(11, 2) == 5
+assert division(4, 2) == 2
+assert division(2, 2) == 1
+assert division(2, 11) == 0

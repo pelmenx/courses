403492fac1b4959044f2c72906a9629f6b3a38ce
pelmenx <vtalantsev@gmail.om>
2021-01-23 16:17:33 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #89 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #89 [Medium].py
@@ -12,3 +12,48 @@
 # --------------------------------------------------------------------------------
 #
 #
+class Binary_tree(object):
+    def __init__(self, arg):
+        super(Binary_tree, self).__init__()
+        self.arg = arg
+        self.left = None
+        self.right = None
+
+
+def is_binary_tree_is_valid(root):
+    def is_binary_tree_is_valid_unside(root_):
+        if not root_.right and not root_.left:
+            yield True
+        if root_.left:
+            if root_.left.arg <= root_.arg:
+                yield from is_binary_tree_is_valid_unside(root_.left)
+            else:
+                yield False
+        if root_.right:
+            if root_.right.arg >= root_.arg:
+                yield from is_binary_tree_is_valid_unside(root_.right)
+            else:
+                yield False
+    check = True
+    for result in is_binary_tree_is_valid_unside(root):
+        check = check and result
+        if check is False:
+            return False
+    return True
+
+
+a = Binary_tree(50)
+b = Binary_tree(25)
+c = Binary_tree(75)
+a.left = b
+a.right = c
+
+assert is_binary_tree_is_valid(a) is True
+
+a = Binary_tree(50)
+b = Binary_tree(25)
+c = Binary_tree(25)
+a.left = b
+a.right = c
+
+assert is_binary_tree_is_valid(a) is False

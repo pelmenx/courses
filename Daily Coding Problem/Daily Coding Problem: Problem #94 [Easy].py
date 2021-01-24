60349be9f74ca85abb6c0332ea1c6a1021f04249
pelmenx <vtalantsev@gmail.om>
2021-01-24 12:02:07 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #94 [Easy].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #94 [Easy].py
@@ -10,3 +10,36 @@
 # --------------------------------------------------------------------------------
 #
 #
+class Node(object):
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.arg = arg
+        self.left = None
+        self.right = None
+
+
+def find_max_sum_of_node(root):
+    def find_max_sum_of_node_inside(root, current_max=0, overall_max=0, right_sum=0, left_sum=0):
+        current_max = max(root.arg, current_max + root.arg)
+        overall_max = max(overall_max, current_max)
+        if not root.left and not root.right:
+            return overall_max
+        if root.right:
+            right_sum = find_max_sum_of_node_inside(root.right, current_max, overall_max)
+        if root.left:
+            left_sum = find_max_sum_of_node_inside(root.left, current_max, overall_max)
+
+        return max(overall_max, left_sum, right_sum)
+    return find_max_sum_of_node_inside(root)
+
+
+a = Node(1)
+assert find_max_sum_of_node(a) == 1
+
+b = Node(2)
+a.left = b
+assert find_max_sum_of_node(a) == 3
+
+c = Node(3)
+a.right = c
+assert find_max_sum_of_node(a) == 4

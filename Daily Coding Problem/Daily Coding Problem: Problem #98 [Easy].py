28abe184944cff2b897edc73ea6b4823629b5732
pelmenx <vtalantsev@gmail.om>
2021-01-24 17:30:15 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #98 [Easy].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #98 [Easy].py
@@ -24,3 +24,37 @@
 # --------------------------------------------------------------------------------
 #
 #
+import copy
+
+
+def exists(array, string,):
+    def check_next_letter(array_inside, string_inside, row, column):
+        if not string_inside:
+            yield True
+        for position in position_list:
+            new_row = row + position[0]
+            new_col = column + position[1]
+            if 0 <= new_row < len(array) and 0 <= new_col < len(array[0]):
+                if array_inside[new_row][new_col] == string_inside[0]:
+                    tmp_array_inside = copy.deepcopy(array_inside)
+                    tmp_array_inside[new_row][new_col] = None
+                    yield from check_next_letter(tmp_array_inside, string_inside[1:], new_row, new_col)
+
+    position_list = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    for i, row_ in enumerate(array):
+        for j, item in enumerate(row_):
+            if item == string[0]:
+                tmp_array = copy.deepcopy(array)
+                tmp_array[i][j] = None
+                for _ in check_next_letter(tmp_array, string[1:], i, j):
+                    return True
+    return False
+
+
+matrix = [['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E']]
+
+assert exists(matrix, "ABCCED") is True
+assert exists(matrix, "SEE") is True
+assert exists(matrix, "ABCB") is False

28f0d33c0c8c70594cdb666d79cb34391eaa5a2d
pelmenx <vtalantsev@gmail.om>
2021-01-25 21:06:59 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #99 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #99 [Medium].py
@@ -14,3 +14,46 @@
 # --------------------------------------------------------------------------------
 #
 #
+def longest_consecutive_element_sequence(array):
+    consecutive_elements_dict = {}
+    consecutive_elements_list = []
+    max_length = 0
+    for item in array:
+        if (item + 1 in consecutive_elements_dict) and (item - 1 in consecutive_elements_dict):
+            if len(consecutive_elements_list[consecutive_elements_dict[item + 1]]) >= len(consecutive_elements_list[consecutive_elements_dict[item - 1]]):
+                consecutive_elements_list[consecutive_elements_dict[item + 1]].append(item)
+                consecutive_elements_list[consecutive_elements_dict[item + 1]].extend(consecutive_elements_list[consecutive_elements_dict[item - 1]])
+                position = consecutive_elements_dict[item - 1]
+                for key in consecutive_elements_list[consecutive_elements_dict[item - 1]]:
+                    consecutive_elements_dict[key] = consecutive_elements_dict[item + 1]
+                consecutive_elements_list[position].clear()
+                consecutive_elements_dict[item] = consecutive_elements_dict[item + 1]
+                if len(consecutive_elements_list[consecutive_elements_dict[item + 1]]) > max_length:
+                    max_length = len(consecutive_elements_list[consecutive_elements_dict[item + 1]])
+            else:
+                consecutive_elements_list[consecutive_elements_dict[item - 1]].append(item)
+                consecutive_elements_list[consecutive_elements_dict[item - 1]].extend(consecutive_elements_list[consecutive_elements_dict[item + 1]])
+                position = consecutive_elements_dict[item + 1]
+                for key in consecutive_elements_list[consecutive_elements_dict[item + 1]]:
+                    consecutive_elements_dict[key] = consecutive_elements_dict[item - 1]
+                consecutive_elements_list[position].clear()
+                consecutive_elements_dict[item] = consecutive_elements_dict[item - 1]
+                if len(consecutive_elements_list[consecutive_elements_dict[item - 1]]) > max_length:
+                    max_length = len(consecutive_elements_list[consecutive_elements_dict[item - 1]])
+        elif item + 1 in consecutive_elements_dict:
+            consecutive_elements_list[consecutive_elements_dict[item + 1]].append(item)
+            consecutive_elements_dict[item] = consecutive_elements_dict[item + 1]
+            if len(consecutive_elements_list[consecutive_elements_dict[item + 1]]) > max_length:
+                max_length = len(consecutive_elements_list[consecutive_elements_dict[item + 1]])
+        elif item - 1 in consecutive_elements_dict:
+            consecutive_elements_list[consecutive_elements_dict[item - 1]].append(item)
+            consecutive_elements_dict[item] = consecutive_elements_dict[item - 1]
+            if len(consecutive_elements_list[consecutive_elements_dict[item - 1]]) > max_length:
+                max_length = len(consecutive_elements_list[consecutive_elements_dict[item - 1]])
+        else:
+            consecutive_elements_dict[item] = len(consecutive_elements_dict)
+            consecutive_elements_list.append([item])
+    return max_length
+
+
+assert longest_consecutive_element_sequence([100, 4, 200, 1, 3, 2]) == 4

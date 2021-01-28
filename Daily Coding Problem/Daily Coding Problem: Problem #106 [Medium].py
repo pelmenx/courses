ec784ed458cbfcf887e09f69fc26501a5bb0364d
pelmenx <vtalantsev@gmail.om>
2021-01-28 13:49:07 +0300

    update

--- Daily Coding Problem/Daily Coding Problem: Problem #106 [Medium].py
+++ Daily Coding Problem/Daily Coding Problem: Problem #106 [Medium].py
@@ -11,3 +11,18 @@
 # --------------------------------------------------------------------------------
 #
 #
+def is_road(array):
+    def check(i):
+        if i == len(array) - 1:
+            return True
+        elif i > len(array):
+            return False
+        elif array[i] == 0:
+            return False
+        else:
+            return check(i + array[i])
+    return check(0)
+
+
+assert is_road([2, 0, 1, 0]) is True
+assert is_road([1, 1, 0, 1]) is False

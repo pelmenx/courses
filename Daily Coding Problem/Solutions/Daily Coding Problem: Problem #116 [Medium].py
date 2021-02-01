617ef91ed70f150c8e377ebe3857a0c34591a194
pelmenx <vtalantsev@gmail.om>
2021-02-01 17:45:52 +0300

    update

--- Daily Coding Problem/Solutions/Daily Coding Problem: Problem #116 [Medium].py
+++ Daily Coding Problem/Solutions/Daily Coding Problem: Problem #116 [Medium].py
@@ -10,3 +10,39 @@
 # --------------------------------------------------------------------------------
 #
 #
+from random import random, randint
+
+
+class Node(object):
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.arg = arg
+        self.left = None
+        self.right = None
+
+
+def make_tree(n):
+    def add_side():
+        side = random()
+        random_node = randint(0, i - 1)
+        if side < 0.5:
+            if node_dict[random_node].left:
+                return add_side()
+            else:
+                node_dict[random_node].left = node_dict[i]
+        elif side > 0.5:
+            if node_dict[random_node].right:
+                return add_side()
+            else:
+                node_dict[random_node].right = node_dict[i]
+        else:
+            return add_side()
+
+    node_dict = {}
+    for i in range(n):
+        node_dict[i] = Node(int(random() * (n * 10)))
+        if i > 0:
+            add_side()
+
+
+make_tree(1000000)
